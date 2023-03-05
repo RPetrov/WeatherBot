a4fa8dbeee338663bac7bbd1423a9e346bcc6b26
Aleksandr Polyakov <master@artwist.ru>
2023-03-05 17:27:20 +0300

    проверка commit, push после изменения версии JDK — artwist-polyakov

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,4 +2,6 @@ fun main(args: Array<String>) {
     println("Hello World!")
     //проверка commit, push
     //проверка commit, push — artwist-polyakov
+    //проверка commit, push после изменения версии JDK — artwist-polyakov
+
 }
