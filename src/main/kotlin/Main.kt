ecacd288cfd6a11363282b1443cbf43f50ac6fe3
nvgurova <yudinanade@yandex.ru>
2023-03-06 09:44:20 +0300

    тест

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello World! Гав")
     //проверка commit, push
     //проверка commit, push — artwist-polyakov
     //проверка commit, push после изменения версии JDK — artwist-polyakov
