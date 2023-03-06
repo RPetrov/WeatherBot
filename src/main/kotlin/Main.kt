31aa13ed604f94955f3a0ea15c2205bf04f7103d
nvgurova <yudinanade@yandex.ru>
2023-03-06 09:49:23 +0300

    Revert "тест"
    This reverts commit ecacd288cfd6a11363282b1443cbf43f50ac6fe3.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World! Гав")
+    println("Hello World!")
     //проверка commit, push
     //проверка commit, push — artwist-polyakov
     //проверка commit, push после изменения версии JDK — artwist-polyakov
