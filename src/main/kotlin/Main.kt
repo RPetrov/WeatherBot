1df9815cf27437d0477a8c7de3aa7aa7b60a97ad
zhek1chan <82vKWTy3NwSc8zn>
2023-03-07 22:25:51 +0500

    created reading of file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
-
 import com.pengrad.telegrambot.TelegramBot
 import com.pengrad.telegrambot.UpdatesListener
+import com.pengrad.telegrambot.model.Location
 import com.pengrad.telegrambot.request.SendMessage
-
 import com.pengrad.telegrambot.response.SendResponse
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVRecord
+import java.io.FileReader
+import java.io.Reader
+import java.nio.file.Files
+import java.nio.file.Paths
 
 
 fun main(args: Array<String>) {
@@ -15,12 +20,16 @@ fun main(args: Array<String>) {
 
 // Register for updates
     bot.setUpdatesListener { updates ->
-        updates.forEach{update -> update.message()?.location()?.let { println(it) } ?: run {
-            val chatId: Long = update.message().chat().id()
-            val response: SendResponse = bot.execute(SendMessage(chatId, "Пришлите, пожалуйста, ваше местоположение"))
-        }}
+        updates.forEach { update ->
+            update.message()?.location()?.let { println(it) } ?: run {
+                val chatId: Long = update.message().chat().id()
+                val response: SendResponse =
+                    bot.execute(SendMessage(chatId, "Пришлите, пожалуйста, ваше местоположение"))
+            }
+        }
         println(updates)
-        UpdatesListener.CONFIRMED_UPDATES_ALL }
+        UpdatesListener.CONFIRMED_UPDATES_ALL
+    }
 
 // Send messages
 
@@ -28,3 +37,43 @@ fun main(args: Array<String>) {
 
 
 }
+
+class BusStopData {
+    val busStops: List<BusStop>
+
+    init {
+
+        //Files.readAllLines(Paths.get("data/perechen-ostanovochnyh-punktov-s-ukazaniem-vida-transporta-i-s-koordinatami-ih-mestopolozheniya.csv"))
+        val `in`: Reader =
+            FileReader("data/perechen-ostanovochnyh-punktov-s-ukazaniem-vida-transporta-i-s-koordinatami-ih-mestopolozheniya.csv")
+
+        val csvFormat: CSVFormat = CSVFormat.DEFAULT.builder()
+            .setHeader("№,Вид транспортного средства,Тип объекта,Наименование остановки,Официальное наименование,Расположение,Маршруты,Координаты")
+            .setSkipHeaderRecord(true)
+            .build()
+
+        val records: Iterable<CSVRecord> = csvFormat.parse(`in`)
+        busStops = records.map { record ->
+            val name: String = record.get("Наименование остановки")
+            val adress: String = record.get("Расположение")
+            val routes: String = record.get("Маршруты")
+            val location: BusStopLocation = record.get("Координаты").let {
+                val locationString = it.split(",")
+                val location =
+                    BusStopLocation(locationString[0].toFloat(), locationString[1].toFloat())
+                return@let location
+            }
+            BusStop(adress, name, routes, location)
+
+        }
+    }
+}
+
+data class BusStop(
+    val adress: String,
+    val name: String,
+    val routes: String,
+    val location: BusStopLocation
+)
+
+data class BusStopLocation(val latitude: Float, val longitude: Float)
