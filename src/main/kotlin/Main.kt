7b3f5e733c264d1b83525dea7233fb3efc26244b
nvgurova <yudinanade@yandex.ru>
2023-03-07 19:39:03 +0300

    Добавлен Бот, отвечаем на сообщения

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,30 @@
+
+import com.pengrad.telegrambot.TelegramBot
+import com.pengrad.telegrambot.UpdatesListener
+import com.pengrad.telegrambot.request.SendMessage
+
+import com.pengrad.telegrambot.response.SendResponse
+
+
 fun main(args: Array<String>) {
-    println("Hello World!")
-    //проверка commit, push
-    //проверка commit, push — artwist-polyakov
-    //проверка commit, push после изменения версии JDK — artwist-polyakov
-    //проверка commit, push
+    // Create your bot passing the token received from @BotFather
+    // Create your bot passing the token received from @BotFather
+    val bot = TelegramBot("6140170233:AAE_j09LC-INAf4cnQ2v7EbSNQv44Q0N27A")
+
+// Register for updates
+
+// Register for updates
+    bot.setUpdatesListener { updates ->
+        updates.forEach{update -> update.message()?.location()?.let { println(it) } ?: run {
+            val chatId: Long = update.message().chat().id()
+            val response: SendResponse = bot.execute(SendMessage(chatId, "Пришлите, пожалуйста, ваше местоположение"))
+        }}
+        println(updates)
+        UpdatesListener.CONFIRMED_UPDATES_ALL }
+
+// Send messages
+
+// Send messages
+
 
 }
