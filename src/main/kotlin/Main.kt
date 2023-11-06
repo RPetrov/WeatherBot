1701a4ef583601265d96526195fa264dd57d8e0f
ra.petrov <ra.petrov@tensor.ru>
2023-11-06 13:05:06 +0300

    add new dependency

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+import com.github.prominence.openweathermap.api.OpenWeatherMapClient
+import com.pengrad.telegrambot.TelegramBot
+
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val token = args.getOrNull(0) ?: run {
+        println("токен!")
+        return
+    }
 }
