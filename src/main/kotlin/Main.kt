168d496ecc9b140a9e9b6513bd7bde37aeaa74b6
Keithcutt <mayerhofnew@gmail.com>
2023-12-25 20:16:02 +0300

    Попытка закоммитить и запушить (не в main).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,9 @@ import com.pengrad.telegrambot.TelegramBot
 fun main(args: Array<String>) {
     val token = args.getOrNull(0) ?: run {
         println("токен!")
+        println("(нет)")
         return
     }
 }
+
+//комит
